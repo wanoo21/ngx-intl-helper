7d67862523d4d0701e557953f06a70fddc08fd41
wanoo21 <ionprodan33@gmail.com>
2023-01-10 10:26:27 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-intl-helper-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'ngx-intl-helper';
+}
