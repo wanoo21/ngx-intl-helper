8b7020b458cb9b055e871719bcb6942e05821d47
wanoo21 <ionprodan33@gmail.com>
2023-01-12 07:41:14 +0200

    fix(helpers): export relativeTime & PluralRules pipes

--- helpers/src/index.ts
+++ helpers/src/index.ts
@@ -2,3 +2,6 @@ export * from './lib/ngx-intl-helper.module';
 
 export * from './lib/display-names.pipe';
 export * from './lib/list-format.pipe';
+export * from './lib/relative-time-format.pipe';
+
+export * from './lib/plural-rules.pipe';
