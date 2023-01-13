1633ac4cf5e54c997d005873257b5fe5ea5f6664
wanoo21 <ionprodan33@gmail.com>
2023-01-13 07:00:44 +0200

    chore(helpers): export all pipes via NgxIntlHelperModule

--- helpers/src/lib/ngx-intl-helper.module.ts
+++ helpers/src/lib/ngx-intl-helper.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
 
 import { DisplayNamesPipe } from "./display-names.pipe";
 import { ListFormatPipe } from "./list-format.pipe";
+import { PluralRulesPipe } from "./plural-rules.pipe";
+import { RelativeTimeFormatPipe } from "./relative-time-format.pipe";
+import { CanonicalLocalesPipe, SupportedValuesOf } from "./methods.pipe";
 
+/**
+ * This module exports all the pipes from the library.
+ */
 @NgModule({
-  imports: [CommonModule, DisplayNamesPipe, ListFormatPipe],
-  exports: [DisplayNamesPipe, ListFormatPipe],
+  imports: [DisplayNamesPipe, ListFormatPipe, PluralRulesPipe, RelativeTimeFormatPipe, CanonicalLocalesPipe, SupportedValuesOf],
+  exports: [DisplayNamesPipe, ListFormatPipe, PluralRulesPipe, RelativeTimeFormatPipe, CanonicalLocalesPipe, SupportedValuesOf]
 })
-export class NgxIntlHelperModule {}
+export class NgxIntlHelperModule {
+}
